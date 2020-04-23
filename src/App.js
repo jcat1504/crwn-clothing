6efb201ef56ac8de6a894af33134af83639425d0
Jess Y <31145424+jcat1504@users.noreply.github.com>
2020-04-23 15:45:48 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx'
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import CheckoutPage from './pages/checkout/checkout.component';
 
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions'
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null; 
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
    this.unsubscribeFromAuth =  auth.onAuthStateChanged(async userAuth => {
      if (userAuth) {
        //we make sure we are passing back the userRef object
@@ -31,19 +30,13 @@ class App extends React.Component {
        userRef.onSnapshot(snapShot => {
           // console.log(snapShot.data());
           //don't just LOG snapShot, we need to extract exact DATA. listed below
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
+            })
           });
-          console.log(this.state);
-       });
-
      }
-     else {
-       this.setState({currentUser: userAuth})
-     };
+       setCurrentUser(userAuth)
      //^^equivalent to null. we need this just in case user logs out
     })
   }
@@ -56,15 +49,33 @@ class App extends React.Component {
   render () {
       return (
     <div>
-      <Header currentUser={this.state.currentUser} />
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
-      <Route path='/signin' component={SignInAndSignUpPage} />
+      <Route exact path='/checkout' component={CheckoutPage} />
+      <Route exact path='/signin' 
+              render={() => 
+              this.props.currentUser ? (
+              <Redirect to='/' />
+              ) : (
+              <SignInAndSignUpPage />
+              )
+            }
+            />
       </Switch>
     </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = createStructuredSelector ({
+  currentUser: selectCurrentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps)(App);
