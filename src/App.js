5100494708376e7ec61d6a467660c1f95cc647c9
Jess Y <31145424+jcat1504@users.noreply.github.com>
2020-05-21 21:20:28 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
-import Header from './components/header/header.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component'
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
